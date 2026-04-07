1188cf7c3f5213ca6505cce0d70b15020e126fd5
RaenRay <raenray2000@outlook.com>
2026-04-07 18:14:19 +0800

    成功绘制三角形

---
+++ shaders/compile.bat
@@ -0,0 +1,7 @@
+@REM C:/VulkanSDK/x.x.x.x/Bin32/glslc.exe shader.vert -o vert.spv
+@REM C:/VulkanSDK/x.x.x.x/Bin32/glslc.exe shader.frag -o frag.spv
+
+D:\Scoop\apps\vulkan\current\Bin\glslc.exe shader.vert -o vert.spv
+D:\Scoop\apps\vulkan\current\Bin\glslc.exe shader.frag -o frag.spv
+
+pause
